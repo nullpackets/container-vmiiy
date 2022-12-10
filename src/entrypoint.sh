55990d1226c23bf83cfc7620cbd9a31ab739eaf1
nullpackets <nullpackets@karmacomputing.co.uk>
2022-12-10 16:43:29 +0000

    added /save endpoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -ex
 
-# Start gunicorn, listening on port 500, access log to stdout
-exec gunicorn -w 4 -b '0.0.0.0:5000' --access-logfile=- 'app:app'
+if [ ${FLASK_DEBUG} ]; then
+
+    flask run --host 0.0.0.0 --port 5000
+else
+    # Start gunicorn, listening on port 500, access log to stdout
+    exec gunicorn -w 4 -b '0.0.0.0:5000' --access-logfile=- 'app:app'
+fi
--- src/web/app.py
+++ src/web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from db import get_db, close_db
 import sqlalchemy
 from logger import log
@@ -11,6 +11,13 @@ app.teardown_appcontext(close_db)
 def index():
     return render_template("index.html")
 
+@app.route("/save", methods=["POST"])
+def save():
+    """Save the mood entered by the user"""
+    date = request.form.get("date")
+    mood = request.form.get("mood")
+    # Save the data into the database
+    return f"Thank you for submitting the date {date}, mood: {mood}"
 
 @app.route("/health")
 def health():
